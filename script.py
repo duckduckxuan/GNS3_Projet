1b1ec540058bf6b30d0ae16e3f380f822a2bd9b9
Xuan <zxuuuan0321@gmail.com>
2024-01-07 17:51:33 +0100

    modified:   script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 import gns3fy
 import requests
 
-# GNS3 server address
-server_url = "http://localhost:3080"
 
 # Create GNS3 server object
+server_url = "http://localhost:3080"
 server = gns3fy.Gns3Connector(server_url)
+
 
 # Check connection to the GNS3 server
 try:
@@ -22,6 +22,8 @@ except requests.exceptions.Timeout:
 except requests.exceptions.RequestException as e:
     print(f"Connection failed, error: {e}")
 
+
+# Get all templates ID in the GNS3
 try:
     response = requests.get(f"{server_url}/v2/templates")
     response.raise_for_status()
@@ -37,21 +39,22 @@ except requests.exceptions.Timeout:
 except Exception as e:
     print(f"An error occurred: {e}")
 
+
 # Create or get the project
 project = gns3fy.Project(name="projet_gns3", connector=server)
 try:
-    project.get()  # Attempt to get the project, if it doesn't exist, it will throw an exception
+    project.get()
 except Exception:
-    project.create()  # If the project doesn't exist, create it
+    project.create()
 
-# Ensure the project has a project_id
-if not project.project_id:
-    print("Failed to get or create a project with a valid project_id.")
 
-# Create a new node in the project
-try:
-    node = gns3fy.Node(project_id=project.project_id, name="R1", node_type="dynamips", template_id="77e90525-2ffb-4b4e-91ec-3d3c86f76392", connector=server)
-    node.create()
-    print(f"Created node {node.name} with ID {node.node_id}")
-except Exception as e:
-    print(f"Failed to create node: {e}")
+# Create some new nodes in the project
+for i in range (6):
+    try:
+        node = gns3fy.Node(project_id=project.project_id, name="router", node_type="dynamips", template_id="77e90525-2ffb-4b4e-91ec-3d3c86f76392", connector=server)
+        node.create()
+        print(f"Created node {node.name} with ID {node.node_id}")
+    except Exception as e:
+        print(f"Failed to create node: {e}")
+
+
