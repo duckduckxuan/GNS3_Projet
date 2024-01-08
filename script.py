01b7a5f44740b2d44c71319bc08fc996346a5ac5
Xuan <zxuuuan0321@gmail.com>
2024-01-08 14:04:53 +0100

    modified:   script.py

--- script.py
+++ script.py
@@ -23,6 +23,7 @@ except requests.exceptions.RequestException as e:
     print(f"Connection failed, error: {e}")
 
 
+'''
 # Get all templates ID in the GNS3
 try:
     response = requests.get(f"{server_url}/v2/templates")
@@ -38,6 +39,7 @@ except requests.exceptions.Timeout:
     print("Timeout error")
 except Exception as e:
     print(f"An error occurred: {e}")
+'''
 
 
 # Create or get the project
@@ -49,12 +51,31 @@ except Exception:
 
 
 # Create some new nodes in the project
+"""
 for i in range (6):
     try:
-        node = gns3fy.Node(project_id=project.project_id, name="router", node_type="dynamips", template_id="77e90525-2ffb-4b4e-91ec-3d3c86f76392", connector=server)
+        node = gns3fy.Node(project_id=project.project_id, name="R1", node_type="dynamips", template_id="77e90525-2ffb-4b4e-91ec-3d3c86f76392", connector=server)
         node.create()
         print(f"Created node {node.name} with ID {node.node_id}")
     except Exception as e:
         print(f"Failed to create node: {e}")
+"""
+
+node1 = gns3fy.Node(project_id=project.project_id, name="R1", node_type="dynamips", template_id="77e90525-2ffb-4b4e-91ec-3d3c86f76392", connector=server)
+
+node2 = gns3fy.Node(project_id=project.project_id, name="R2", node_type="dynamips", template_id="77e90525-2ffb-4b4e-91ec-3d3c86f76392", connector=server)
 
 
+# 假设每个节点都有至少一个接口
+node1_adapter_number = 0  # 第一个节点的适配器编号
+node1_port_number = 5000     # 第一个节点的端口编号
+node2_adapter_number = 0  # 第二个节点的适配器编号
+node2_port_number = 5001     # 第二个节点的端口编号
+
+# 创建连接
+link = gns3fy.Link(project_id=project.project_id, nodes=[node1, node2], node_interfaces=["FastEthernet0/0", "FastEthernet0/0"], link_type="ethernet")
+
+
+# 获取连接信息
+link.get()
+print(f"Link created between {node1.name} and {node2.name}")
