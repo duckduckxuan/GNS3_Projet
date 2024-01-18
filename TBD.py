26c992a1b813c05590c19c861956c4607997b6d8
Xuan <zxuuuan0321@gmail.com>
2024-01-18 23:29:58 +0100

    deleted:    R1_config.cfg 	deleted:    R2_config.cfg 	deleted:    R3_config.cfg 	deleted:    R4_config.cfg 	deleted:    R5_config.cfg 	deleted:    R6_config.cfg 	deleted:    R7_config.cfg 	modified:   TBD.py 	deleted:    config_final/try.py 	deleted:    example.cfg 	deleted:    main.py

--- TBD.py
+++ TBD.py
@@ -1,7 +1,11 @@
 import json
 
 def generate_router_config(router_info):
-    """Generate router configuration based on the provided information."""
+    # List of routers using different protocol
+    rip = ["R1", "R2", "R3", "R4", "R5", "R6", "R7"]
+    ospf = ["R8", "R9", "R10", "R11", "R12", "R13", "R14"]
+
+    # Generate router configuration based on the provided information.
     config = [
         "!\r"*3,
         "!",
@@ -13,42 +17,91 @@ def generate_router_config(router_info):
         "!",
         "boot-start-marker",
         "boot-end-marker",
-        "!\r"*3,
+        "!\r"*2 + "!",
         "no aaa new-model",
         "no ip icmp rate-limit unreachable",
         "ip cef",
-        "!\r"*6,
+        "!\r"*5 + "!",
         "no ip domain lookup",
         "ipv6 unicast-routing",
         "ipv6 cef",
-        "!\r"*2,
+        "!\r!",
         "multilink bundle-name authenticated",
-        "!\r"*9,
+        "!\r"*8 + "!",
         "ip tcp synwait-time 5",
-        "!\r"*12,
+        "!\r"*11 + "!",
     ]
 
     # Configure Loopback Interface
     config.append("interface Loopback0")
-    config.append(f" ip address {router_info['loopback']}")
+    config.append(" no ip address")
+    config.append(f" ipv6 address {router_info['loopback']}")
+    config.append(" ipv6 enable")
+
+    if router_info['name'] in rip:
+        config.append(" ipv6 rip 2001 enable")
+    else:
+        config.append(" ipv6 ospf 2002 area 0")
+
     config.append("!")
 
-    # Configure each interface
+    # Configure each interface (需要修改，未完成)
     for interface in router_info['interfaces']:
         config.append(f"interface {interface['name']}")
         config.append(f" ip address {interface['ip_address']}")
         config.append(" no shutdown")
         config.append("!")
 
-    reste = [
+    # Configure the first part of ending infomation
+    partie_1 = [
         "!",
         "ip forward-protocol nd",
-        "!\r"*2,
+        "!\r!",
         "no ip http server",
         "no ip http secure-server",
-        "!",
-        
+        "!"
     ]
+
+    for i in partie_1:
+        config.append(i)
+
+    # Configure part of protocol
+    if router_info['name'] in rip:
+        config.append("ipv6 router rip 2001")
+        config.append(" redistribute connected")
+    else:
+        config.append("ipv6 router ospf 2002")
+        config.append(f" router-id {router_info['id']}") # json文件里加router id (待完成)
+
+    if router_info['name']=="R8":
+        config.append(" passive-interface GigabitEthernet1/0")
+
+    if router_info['name']=="R9":
+        config.append(" passive-interface FastEthernet0/0")
+
+    # Configure the second part of ending information
+    partie_2 = [
+        "!\r"*3 + "!",
+        "control-plane",
+        "!\r!",
+        "line con 0",
+        " exec-timeout 0 0",
+        " privilege level 15",
+        " logging synchronous",
+        " stopbits 1",
+        "line aux 0",
+        " exec-timeout 0 0",
+        " privilege level 15",
+        " logging synchronous",
+        " stopbits 1",
+        "line vty 0 4",
+        " login",
+        "!\r!",
+        "end"
+    ]
+
+    for i in partie_2:
+        config.append(i)
 
     return "\n".join(config)
 
@@ -61,3 +114,5 @@ for router in routers['AS'][0]['routers']:
     config = generate_router_config(router)
     with open(f"{router['name']}_config.cfg", 'w') as file:
         file.write(config)
+
+# 移动生成文件至GNS3 project文件夹（未完成）
