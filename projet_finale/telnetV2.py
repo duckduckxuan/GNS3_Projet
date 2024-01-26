8255e3f502f85fb4475fbdac011fc0e9dd047324
CCtuhulu <wajdimhamdi999@gmail.com>
2024-01-26 13:16:28 +0100

    telnetV2 manque juste traitement bgp

---
+++ projet_finale/telnetV2.py
@@ -0,0 +1,90 @@
+import gns3fy
+import telnetlib
+import time
+import json
+from allocate_addres import *
+from telnet_configuration import *
+
+
+
+nodes_ports = {}
+
+def get_nodes(project_name):
+
+    server = gns3fy.Gns3Connector("http://localhost:3080")
+
+    projet = gns3fy.Project(name=project_name, connector=server)
+    
+    projet.get()
+    projet.open()
+
+    for node in projet.nodes:
+        node.get()
+        nodes_ports[node.name] = node.console
+
+def telnet_write(config,port):
+    
+    try:
+        print(port)
+        tn = telnetlib.Telnet('localhost',port)
+        time.sleep(1)
+        tn.write(b"\r") #To start writing
+        time.sleep(1)
+
+        for command in config:
+            tn.write(command.encode())
+            tn.write(b"\r")
+
+        time.sleep(1)
+        tn.write(b"end\rwrite\r\r")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+get_nodes("TEMPLATE GNS3")
+#print(nodes_ports)
+
+
+with open('projet_finale/router_infos_TBD.json', 'r') as file:
+    data = json.load(file)
+
+all_as = [AS(as_info['number'], as_info['IP_range'], as_info['loopback_range'], as_info['protocol'], as_info['routers']) 
+          for as_info in data['AS']]
+
+as_mapping = {}
+for as_index in all_as:
+    for router in as_index.routers:
+        as_mapping[router.name] = as_index.number
+
+all_routers = [router for as_index in all_as for router in as_index.routers]
+connections_matrix_name = generate_connections_matrix_name(all_routers, as_mapping)
+#print(connections_matrix_name)
+connections_matrix = generate_connections_matrix(all_routers, as_mapping)
+routers_info = generate_routers_dict(all_as)
+
+connection_counts = {"111": 0, "112": 0, "border": 0}
+for conn in connections_matrix:
+    connection_counts[conn[1]] += 1
+
+
+for as_index in all_as:
+    for router in as_index.routers:
+        generate_interface_addresses(router.name, router.interfaces, connections_matrix, connection_counts)
+
+for as_index in all_as:
+    for router in as_index.routers:
+        router_loopback = generate_loopback(router.name, as_index.loopback_range)
+        router_id = generate_router_id(router.name)
+        config = []
+
+
+        config.extend(config_loopback(router_loopback, as_index.protocol))
+        config.extend(config_interface(router.interfaces, as_index.protocol, router, connections_matrix_name))
+
+        # config.extend(config_bgp(router, router_id, all_routers, connections_matrix_name, routers_info))
+
+        print(config)
+        telnet_write(config,nodes_ports[router.name])
+
+
+        
---
+++ projet_finale/telnet_configuration.py
@@ -0,0 +1,163 @@
+from allocate_addres import *
+import ipaddress
+
+# Configure Loopback Interface(已完成)
+def config_loopback(ip_loopback, protocol):
+    config = []
+
+    config.append("enable")
+    config.append("conf t")
+    config.append("ipv6 unicast-routing")
+    config.append("interface Loopback0")
+    config.append("no ip address")
+    config.append(f"ipv6 address {ip_loopback}")
+    config.append("ipv6 enable")
+
+    if protocol == "RIP":
+        config.append(" ipv6 rip 2001 enable")
+    if protocol == "OSPF":
+        config.append(" ipv6 ospf 2002 area 0")
+
+    config.append("end")
+
+    return config
+
+
+# Configure each interface(已完成)
+def config_interface(interfaces, protocol, router, connections_matrix_name):
+    config = []
+
+    for interface in interfaces:
+        config.append("conf t")
+
+        config.append(f"interface {interface['name']}")
+        config.append("no ip address")
+
+        if interface['neighbor'] == "None":
+            config.append("shutdown")
+
+            if interface['name'] == "FastEthernet0/0":
+                config.append("duplex full")
+            else:
+                config.append("negotiation auto")
+
+        else:
+            if interface['name'] == "FastEthernet0/0":
+                config.append("duplex full")
+            else:
+                config.append("negotiation auto")
+
+            ipv6_address = interface.get('ipv6_address', '')  # Get the IPv6 address from the interface dict
+
+            if ipv6_address:
+                config.append(f"ipv6 address {ipv6_address}")
+                config.append("ipv6 enable")
+                config.append("no shutdown")
+
+                if protocol == "RIP" and not ipv6_address.startswith("2001:192:170:"):
+                    config.append("end")
+                    config.append('conf t')
+                    config.append("ipv6 router rip 2001")
+                    config.append("redistribute connected")
+                    config.append("end")
+
+                    config.append("conf t")
+                    config.append(f"interface {interface['name']}")
+                    config.append("ipv6 rip 2001 enable")
+
+                elif protocol == "OSPF":  # Changed to elif for better practice
+                    config.append("end")
+                    config.append('conf t')
+                    config.append("ipv6 router ospf 2002")
+                    config.append(f"router-id {router.name[1:]}.{router.name[1:]}.{router.name[1:]}.{router.name[1:]}")
+                    config.append("end")
+
+                    config.append("conf t")
+                    config.append(f"interface {interface['name']}")
+                    config.append(" ipv6 ospf 2002 area 0")
+
+        config.append("end")
+
+    return config  # Moved return statement outside the loop
+
+
+# Configure bgp neighbor
+def config_bgp(router, router_id, routers, connections_matrix_name, routers_dict):
+    config = []
+    current_as = routers_dict[router.name]['AS']
+    neighbor_liste = []
+
+    config.append(f"router bgp {current_as}")
+    config.append(f" bgp router-id {router_id}")
+    config.append(" bgp log-neighbor-changes")
+    config.append(" no bgp default ipv4-unicast")
+
+    if router.router_type == "eBGP":
+        neighbor_ip = None
+
+        for elem in connections_matrix_name:
+            ((r1, r2), state) = elem
+
+            if state == 'border':
+                if router.name == r1:
+                    neighbor = r2
+                elif router.name == r2:
+                    neighbor = r1
+                else:
+                    neighbor = None
+
+                if neighbor:
+                    for routeur in routers:
+                        if routeur.name == neighbor:
+                            for interface in routeur.interfaces:
+                                if interface['neighbor'] == router.name:
+                                    print(router.name)
+                                    neighbor_ip = interface.get('ipv6_address', '')
+                                    print(f"找到邻居ip了{neighbor_ip}")
+                                    break
+
+                    if neighbor_ip:
+                        as_number = routers_dict[neighbor]['AS']
+                        config.append(f" neighbor {neighbor_ip[:-3]} remote-as {as_number}")
+                        neighbor_liste.append(neighbor_ip[:-3])
+
+    for routeur_name, routeur_info in routers_dict.items():
+        if routeur_name != router.name and routeur_info['AS'] == current_as:
+            config.append(f" neighbor {routeur_info['loopback'][:-4]} remote-as {routeur_info['AS']}")
+            config.append(f" neighbor {routeur_info['loopback'][:-4]} update-source Loopback0")
+            neighbor_liste.append(routeur_info['loopback'][:-4])
+
+    config.append(" !")
+    config.append(" address-family ipv4")
+    config.append(" exit-address-family")
+    config.append(" !")
+    config.append(" address-family ipv6")
+
+    # Announce neighbor subnet
+    networks = []
+    for interface in router.interfaces:
+        ip_addr = interface.get('ipv6_address', '')
+        if ip_addr:
+            try:
+                network = ipaddress.IPv6Network(ip_addr, strict=False)
+                networks.append(network)
+            except ValueError:
+                print(f"Invalid IPv6 addresse: {ip_addr}")
+
+    # Sort subnet
+    networks.sort(key=lambda net: (net.network_address, net.prefixlen))
+
+    # Add subnets to configuration
+    for network in networks:
+        config.append(f"  network {str(network)}")
+
+    # Activate neighbor IP loopback
+    for ip_neighbor in neighbor_liste:
+        config.append(f"  neighbor {ip_neighbor} activate")
+
+    config.append(" exit-address-family")
+    config.append("!")
+
+    return config
+
+      
